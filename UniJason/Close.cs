666e2805b5eac513fa9e555328b31ba1561e832c
Juan Gómez-Martinho <jugomezm@ucm.es>
2017-11-03 10:02:16 +0100

    UniJason initial Script
    Un script se ejecuta cuando se inicia el juego y levanta jedit. Queda probar que ejecute directamente el server, pero es un comienzo.

---
+++ UniJason/Close.cs
@@ -0,0 +1,1 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Close : MonoBehaviour {  	// Use this for initialization 	void Start () { 		 	} 	 	// Update is called once per frame 	void Update () { 		 	}     void OnApplicationQuit()     {         Debug.Log("Application ending after " + Time.time + " seconds");     } } 
