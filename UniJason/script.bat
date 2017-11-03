865a9cf136b12753f24fc8a27158cfeeb9d17300
Koldur <koldur.c@gmail.com>
2017-11-03 11:07:06 +0100

    Cambios en el script
    Me voy acercando a que se ejecute el MAS sin necesidad del jedit, pero el script da problemas

--- UniJason/script.bat
+++ UniJason/script.bat
@@ -1,7 +1,1 @@
-if exist EmptyFile.txt (
-    rem file exists
-) else (
-   	type NUL > EmptyFile.txt
-	start /wait javaw -jar "%~dp0jason\jedit\jedit.jar"
-	del EmptyFile.txt
-)
+javaw jason.infra.centralised.RunCentralisedMAS "%~dp0JasonIsoMonks-master\agentes.mas2j
