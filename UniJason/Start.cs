dde2d653df06db2ca5715683836526a4d12e8575
Juan Gómez-Martinho <jugomezm@ucm.es>
2017-11-05 15:09:42 +0100

    script unity corregido
    Ahora se ejecuta sin problemas en directorios con espacios

--- UniJason/Start.cs
+++ UniJason/Start.cs
@@ -1,1 +1,21 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEditor; using System.Diagnostics;  [InitializeOnLoad] public class Start {     static Start()     {         UnityEngine.Debug.Log("Up and running");         Process myProcess = new Process();         myProcess.StartInfo.UseShellExecute = false;         myProcess.StartInfo.RedirectStandardOutput = true;         myProcess.StartInfo.FileName = "C:\\Windows\\system32\\cmd.exe";         myProcess.StartInfo.Arguments = "/c" + Application.dataPath + "/UniJason/script.bat";         myProcess.Start();         System.Threading.Thread.Sleep(5000);     } }
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+[InitializeOnLoad]
+public class Start {
+    static Start()
+    {
+        UnityEngine.Debug.Log("Up and running");
+        Process myProcess = new Process();
+        myProcess.StartInfo.UseShellExecute = false;
+        myProcess.StartInfo.RedirectStandardOutput = true;
+        myProcess.StartInfo.FileName = "C:\\Windows\\system32\\cmd.exe";
+        string r = "/c" + "\"" + Application.dataPath + "/UniJason/script.bat" + "\"";
+        myProcess.StartInfo.Arguments = r;
+        myProcess.Start();
+        System.Threading.Thread.Sleep(5000);
+    }
+}
