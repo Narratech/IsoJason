cebcb4d413572d8b1d6ad1e340f43e5b743e607b
Koldur <koldur.c@gmail.com>
2017-11-19 00:26:29 +0100

    .bat actualizado

--- UniJason/script.bat
+++ UniJason/script.bat
@@ -3,5 +3,7 @@ if exist EmptyFile.txt (
 ) else (
    	type NUL > EmptyFile.txt
 	start /wait javaw -jar "%~dp0server.jar"
+=======
+	start /wait javaw -jar "%~dp0jason\jedit\jedit.jar"
+>>>>>>> d4938ae9be63d7e6104ca1f89ba433d11f55e3d5
 	del EmptyFile.txt
-)
